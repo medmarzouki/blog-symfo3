72491b059a0ae0cb052174172b35d277b1bbda44
mohamedmarzouki <mohamedmarzouki9@gmail.com>
2019-01-22 15:55:29 +0100

    rectification des routes de portfolio

--- src/ClientBundle/Controller/PortfolioController.php
+++ src/ClientBundle/Controller/PortfolioController.php
@@ -4,6 +4,7 @@ namespace ClientBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 
 /**
  * @Route("/portfolio", name="portfolio")
@@ -14,53 +15,47 @@ class PortfolioController extends Controller
 {
     /**
      * @Route("/", name="portfolio.index")
-     * @Method()
+     * @Method({"GET"})
      */
     public function indexAction()
     {
-        return $this->render('ClientBundle:Portfolio:index.html.twig', array(
-            // ...
-    ));
-    }
-
-    /**
-     * @Route("/{id}")
-     */
-    public function editAction($id)
-    {
-        return $this->render('ClientBundle:Portfolio:edit.html.twig', array(
-            // ...
-        ));
-    }
-
-    /**
-     * @Route("/create")
-     */
-    public function createAction()
-    {
-        return $this->render('ClientBundle:Portfolio:create.html.twig', array(
-            // ...
-        ));
+        return $this->render('ClientBundle:Portfolio:index.html.twig', array());
     }
 
     /**
      * @Route("/{id}/edit")
+     * @Method({"GET", "POST"})
+     */
+    public function editAction($id)
+    {
+        return $this->render('ClientBundle:Portfolio:edit.html.twig', array());
+    }
+
+    /**
+     * @Route("/create")
+     * @Method({"GET", "POST"})
+     */
+    public function createAction()
+    {
+        return $this->render('ClientBundle:Portfolio:create.html.twig', array());
+    }
+
+    /**
+     * @Route("/{id}/delete")
+     * @Method({"DELETE"})
      */
     public function deleteAction($id)
     {
-        return $this->render('ClientBundle:Portfolio:delete.html.twig', array(
-            // ...
-        ));
+        return $this->render('ClientBundle:Portfolio:delete.html.twig', array());
     }
 
     /**
      * @Route("/{id}/show")
+     * @Method({"GET"})
      */
     public function showAction($id)
     {
-        return $this->render('ClientBundle:Portfolio:show.html.twig', array(
-            // ...
-        ));
+        return $this->render('ClientBundle:Portfolio:show.html.twig', array());
     }
 
 }
